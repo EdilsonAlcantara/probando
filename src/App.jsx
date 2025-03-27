e64686abf4dac5e30ce1e05de8e9fcc135d68edf
Edilson Alcantara <edilsona@bellon.com.do>
2025-03-27 11:39:52 -0400

    modificando el componente para que muestre las aplicaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useEffect, useState } from 'react'
+import datos from './Datos/datos.json'
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router';
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [aplicaciones, setAplicaciones] = useState([]);
+
+    useEffect(() => {
+        const cargarDatos = async () => {
+            setAplicaciones(datos);
+        };
+        cargarDatos();
+    }, []);
+
+    return (
+        <Container className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh', backgroundColor: '' }}>
+            <Row className="mb-4 w-100">
+                <Col className="text-center">
+                    <h1>Modulos Disponibles</h1>
+                </Col>
+            </Row>
+            <Row className="justify-content-center">
+                {aplicaciones.map((app, index) => (
+                    <Col key={index} xs={12} sm={6} md={6} className="mb-5">
+                        <Card className="text-center zoom-card">
+                            <Card.Body>
+                                <a href={app.link} target="_blank" rel="noopener noreferrer">
+                                    <div className="mb-3">
+                                        <img src={"/" + app.icon + '.png'} alt={app.name} className="img-fluid" style={{ width: '150px', height: '150px' }} />
+                                    </div>
+                                    <Card.Title>{app.name}</Card.Title>
+                                </a>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+    )
+
 }
 
 export default App
