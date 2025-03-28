4ae1c7d3a9233efd6d1274bf66d2999710228e0f
Edilson Alcantara <edilsona@bellon.com.do>
2025-03-28 11:47:01 -0400

    covirtiendo rows en componentes independientes

--- src/Modulos/Modulos.jsx
+++ src/Modulos/Modulos.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
+import { Col, Container, Form, Row } from 'react-bootstrap'
 import { ModulosProveedor } from './Controles/modulosContexto'
 import ListadoModulos from './Vistas/ListadoModulos'
+import { Check } from 'react-bootstrap-icons'
+import TituloModulo from './Vistas/TituloModulo'
 
 export default function Modulos() {
     return (
         <ModulosProveedor>
             <Container fluid>
-                <Row className="mb-2" style={{ position: 'sticky', top: 0, zIndex: 1300, background: '#00b7c3' }}>
-                    <Col className="text-center text-white my-2">
-                        <h1>Módulos</h1>
-                    </Col>
-                </Row>
+                <TituloModulo />
                 <ListadoModulos />
             </Container>
         </ModulosProveedor>
