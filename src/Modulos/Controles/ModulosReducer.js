0e1d28a0e03035e310934663b5daea2fa9e74243
Edilson Alcantara <edilsona@bellon.com.do>
2025-03-28 11:49:52 -0400

    agregando console log en el reducer

--- src/Modulos/Controles/ModulosReducer.js
+++ src/Modulos/Controles/ModulosReducer.js
@@ -2,6 +2,8 @@ import { EstadoInicialModulos } from "../Modelos/EstadoInicialModulos";
 
 export const modulosReducer = (state = EstadoInicialModulos, action) => {
 
+    console.log('modulosReducer', state);
+
     if (action.type === 'setPropiedad') {
         return { ...state, [action.payload.id]: action.payload.value };
     }
